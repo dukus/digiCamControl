ba7f8db1ae42449dba7e4691b7894432fb0a2d1a
Duka Istvan <dukusi@gmail.com>
2014-07-28 22:11:54 +0300

    WindowsService for WCF

---
+++ CameraControl.Service/CameraWindowsService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+
+namespace CameraControl.Service
+{
+    public class CameraWindowsService : ServiceBase
+    {
+        public ServiceHost serviceHost = null;
+
+        public CameraWindowsService()
+        {
+            // Name the Windows Service
+            ServiceName = "CameraWindowsService";
+        }
+
+        public static void Main()
+        {
+            ServiceBase.Run(new CameraWindowsService());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+            }
+
+            // Create a ServiceHost for the CalculatorService type and 
+            // provide the base address.
+            serviceHost = new ServiceHost(typeof(CameraService));
+
+            // Open the ServiceHostBase to create listeners and start 
+            // listening for messages.
+            serviceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (serviceHost != null)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ CameraControl.Service/ServiceInstaller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace CameraControl.Service
+{
+    [RunInstaller(true)]
+    public class ProjectInstaller : Installer
+    {
+        private ServiceProcessInstaller process;
+        private ServiceInstaller service;
+
+        public ProjectInstaller()
+        {
+            process = new ServiceProcessInstaller();
+            process.Account = ServiceAccount.LocalSystem;
+            service = new ServiceInstaller();
+            service.ServiceName = "CameraWindowsService";
+            Installers.Add(process);
+            Installers.Add(service);
+        }
+    }
+}
