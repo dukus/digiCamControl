832605ac6207bcb36102c8e1a9af28321e63e227
Duka Istvan <dukusi@gmail.com>
2015-04-04 14:29:04 +0300

    Horizontal scroll for thumb view

---
+++ CameraControl/Classes/HorizontalScrollBehavior.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace CameraControl.Classes
+{
+    public class HorizontalScrollBehavior : Behavior<ItemsControl>
+    {
+        /// <summary>
+        /// A reference to the internal ScrollViewer.
+        /// </summary>
+        private ScrollViewer ScrollViewer { get; set; }
+
+        /// <summary>
+        /// By default, scrolling down on the wheel translates to right, and up to left.
+        /// Set this to true to invert that translation.
+        /// </summary>
+        public bool IsInverted { get; set; }
+
+        /// <summary>
+        /// The ScrollViewer is not available in the visual tree until the control is loaded.
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            AssociatedObject.Loaded -= OnLoaded;
+
+            ScrollViewer = VisualTreeHelpers.FindVisualChild<ScrollViewer>(AssociatedObject);
+
+            if (ScrollViewer != null)
+            {
+                ScrollViewer.PreviewMouseWheel += OnPreviewMouseWheel;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (ScrollViewer != null)
+            {
+                ScrollViewer.PreviewMouseWheel -= OnPreviewMouseWheel;
+            }
+        }
+
+        private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var newOffset = IsInverted ?
+                ScrollViewer.HorizontalOffset + e.Delta/100 :
+                ScrollViewer.HorizontalOffset - e.Delta/100;
+
+            ScrollViewer.ScrollToHorizontalOffset(newOffset);
+        }
+    }
+}
---
+++ CameraControl/Classes/VisualTreeHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace CameraControl.Classes
+{
+    public class VisualTreeHelpers
+    {
+        public static T FindVisualChild<T>(DependencyObject obj)
+            where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T) child;
+                else
+                {
+                    var childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+    }
+}
