6035abc4f767e6c03f3b362091daf5d5edd68c82
Duka Istvan <dukusi@gmail.com>
2015-06-12 14:46:18 +0300

    Custom device support

---
+++ CameraControl.Devices/Custom/DeviceDescription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CameraControl.Devices.Custom
+{
+    public class DeviceDescription
+    {
+        public string Model { get; set; }
+        public string Description { get; set; }
+        public string BaseDevice { get; set; }
+    }
+
+
+}
