54b4e9f38e9797a903570da7f497beb38d1ea79e
Duka Istvan <dukusi@gmail.com>
2015-06-13 23:00:45 +0300

    Beta release 1.2.99.12

--- CameraControl.Core/Properties/AssemblyInfo.cs
+++ CameraControl.Core/Properties/AssemblyInfo.cs
@@ -68,5 +68,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.99.10")]
-[assembly: AssemblyFileVersion("1.2.99.10")]
+[assembly: AssemblyVersion("1.2.99.12")]
+[assembly: AssemblyFileVersion("1.2.99.12")]
