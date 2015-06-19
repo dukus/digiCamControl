e90098ad4bd65a21ed032c8919636435317ccc6a
Duka Istvan <dukusi@gmail.com>
2015-06-19 16:04:00 +0300

    Rotate transform part I

---
+++ CameraControl.Plugins/ImageTransformPlugins/RotateTransform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using CameraControl.Core.Classes;
+using CameraControl.Core.Interfaces;
+
+namespace CameraControl.Plugins.ImageTransformPlugins
+{
+    public class RotateTransform : IImageTransformPlugin
+    {
+        public string Name
+        {
+            get { return "Rotate"; }
+        }
+
+        public string Execute(FileItem item, string infile, string dest, ValuePairEnumerator configData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserControl GetConfig(ValuePairEnumerator configData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
