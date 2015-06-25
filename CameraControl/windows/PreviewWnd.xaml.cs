933d34d2d4423d3b35b85c00f4c473cea191c4f9
Duka Istvan <dukusi@gmail.com>
2015-06-25 16:52:17 +0300

    Preview transform plugin

---
+++ CameraControl/windows/PreviewWnd.xaml.cs
@@ -0,0 +1,16 @@
+﻿using CameraControl.Core;
+
+namespace CameraControl.windows
+{
+    /// <summary>
+    /// Interaction logic for PreviewWnd.xaml
+    /// </summary>
+    public partial class PreviewWnd 
+    {
+        public PreviewWnd()
+        {
+            InitializeComponent();
+            ServiceProvider.Settings.ApplyTheme(this);
+        }
+    }
+}
