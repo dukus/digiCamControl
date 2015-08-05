51c977c9fa631edd885e1d362e1513d9be256ef0
Duka Istvan <dukusi@gmail.com>
2015-08-05 17:12:53 +0300

    New nuget package

--- CameraControl.Core/Properties/AssemblyInfo.cs
+++ CameraControl.Core/Properties/AssemblyInfo.cs
@@ -68,5 +68,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.99.20")]
-[assembly: AssemblyFileVersion("1.2.99.20")]
+[assembly: AssemblyVersion("1.2.99.21")]
+[assembly: AssemblyFileVersion("1.2.99.21")]
