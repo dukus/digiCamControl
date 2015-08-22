1b6638ca0f6b305e50ed50a174e21354ac8b8846
Duka Istvan <dukusi@gmail.com>
2015-08-22 17:18:52 +0300

    Improved transform plugin

--- CameraControl.Plugins/AutoExportPlugins/TransformPlugin.cs
+++ CameraControl.Plugins/AutoExportPlugins/TransformPlugin.cs
@@ -14,15 +14,37 @@ namespace CameraControl.Plugins.AutoExportPlugins
             configData.IsRedy = false;
             configData.IsError = false;
             var filename = item.FileName;
-
+            var conf = new TransformPluginViewModel(configData);
             var outfile = Path.GetTempFileName();
-            AutoExportPluginHelper.ExecuteTransformPlugins(item, configData, outfile);
-            // wait for file to be not locked
-            PhotoUtils.WaitForFile(filename);
-            File.Copy(outfile, filename, true);
-            File.Delete(outfile);
-            item.IsLoaded = false;
-            item.RemoveThumbs();
+            outfile = AutoExportPluginHelper.ExecuteTransformPlugins(item, configData, outfile);
+            if (conf.CreateNew)
+            {
+                string newFile = Path.Combine(Path.GetDirectoryName(filename),
+                    Path.GetFileNameWithoutExtension(filename) + "_transformed" + Path.GetExtension(outfile));
+                File.Copy(outfile, newFile, true);
+                if (ServiceProvider.Settings.DefaultSession.GetFile(newFile)==null)
+                {
+                    FileItem im = new FileItem(newFile);
+                    var i = ServiceProvider.Settings.DefaultSession.Files.IndexOf(item);
+                    if (ServiceProvider.Settings.DefaultSession.Files.Count - 1 == i)
+                    {
+                        ServiceProvider.Settings.DefaultSession.Files.Add(im);
+                    }
+                    else
+                    {
+                        ServiceProvider.Settings.DefaultSession.Files.Insert(i + 1, im);
+                    }
+                    
+                }
+            }
+            else
+            {
+                // wait for file to be not locked
+                PhotoUtils.WaitForFile(filename);
+                File.Copy(outfile, filename, true);
+                item.IsLoaded = false;
+                item.RemoveThumbs();
+            }
             // remove unused file
             if (outfile != item.FileName)
             {
--- CameraControl.Plugins/AutoExportPlugins/TransformPluginViewModel.cs
+++ CameraControl.Plugins/AutoExportPlugins/TransformPluginViewModel.cs
@@ -8,10 +8,28 @@ namespace CameraControl.Plugins.AutoExportPlugins
 {
     public class TransformPluginViewModel : BasePluginViewModel
     {
+       
         public TransformPluginViewModel(AutoExportPluginConfig config)
         {
             _config = config;
         }
+
+        public bool CreateNew
+        {
+            get { return _config.ConfigData["CreateNew"] == "True"; }
+            set
+            {
+                _config.ConfigData["CreateNew"] = value.ToString();
+                RaisePropertyChanged(() => OverWrite);
+            }
+        }
+
+        public bool OverWrite
+        {
+            get { return !CreateNew; }
+
+        }
+
 
         public TransformPluginViewModel()
         {
