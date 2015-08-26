534b3a0a7e16c951527961bd051cb6df4b6259f0
Duka Istvan <dukusi@gmail.com>
2015-08-27 00:06:46 +0300

    DropboxPlugin part II

--- CameraControl.Plugins/AutoExportPlugins/DropboxPlugin.cs
+++ CameraControl.Plugins/AutoExportPlugins/DropboxPlugin.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Controls;
+using CameraControl.Core;
 using CameraControl.Core.Classes;
 using CameraControl.Core.Interfaces;
+using CameraControl.Devices;
 
 namespace CameraControl.Plugins.AutoExportPlugins
 {
@@ -12,6 +16,9 @@ namespace CameraControl.Plugins.AutoExportPlugins
     {
         public bool Execute(FileItem item, AutoExportPluginConfig configData)
         {
+            Thread thread = new Thread(() => Send(item, configData));
+            thread.Start();
+            return true;
             return true;
         }
 
@@ -27,6 +34,36 @@ namespace CameraControl.Plugins.AutoExportPlugins
                 DataContext = new DropboxViewModel(configData)
             };
             return cnt;
+        }
+
+        public void Send(FileItem item, AutoExportPluginConfig configData)
+        {
+            try
+            {
+                var filename = item.FileName;
+                configData.IsRedy = false;
+                configData.IsError = false;
+                var conf = new DropboxViewModel(configData);
+
+                var outfile = Path.Combine(Path.GetTempPath(), Path.GetFileName(filename));
+                outfile = AutoExportPluginHelper.ExecuteTransformPlugins(item, configData, outfile);
+
+                conf.Upload(outfile,ServiceProvider.Settings.DefaultSession.Name);
+
+                // remove unused file
+                if (outfile != item.FileName)
+                {
+                    PhotoUtils.WaitForFile(outfile);
+                    File.Delete(outfile);
+                }
+            }
+            catch (Exception exception)
+            {
+                Log.Error("Error send dropbox file", exception);
+                configData.IsError = true;
+                configData.Error = exception.Message;
+            }
+            configData.IsRedy = true;
         }
     }
 }
--- CameraControl.Plugins/AutoExportPlugins/DropboxViewModel.cs
+++ CameraControl.Plugins/AutoExportPlugins/DropboxViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
 using System.Windows;
 using CameraControl.Core;
 using CameraControl.Core.Classes;
+using CameraControl.Devices;
 using DropNet;
 using GalaSoft.MvvmLight.Command;
 
@@ -16,8 +18,52 @@ namespace CameraControl.Plugins.AutoExportPlugins
         private const string Server = "http://localhost:5514/";
         DropNetClient _client = new DropNetClient("71jiamqc1znjurb", "yt7p9rxiq87cpnd");
         private MiniWebServer _miniWebServer ;
+        private bool _isLogedIn;
 
-        public RelayCommand LoginCommand { get; set; } 
+        public RelayCommand LoginCommand { get; set; }
+        public RelayCommand LogoutCommand { get; set; }
+
+        public string AccessToken
+        {
+            get { return _config.ConfigData["AccessToken"]; }
+            set
+            {
+                _config.ConfigData["AccessToken"] = value;
+                LoadData();
+            }
+        }
+
+        public string Secret
+        {
+            get { return _config.ConfigData["Secret"]; }
+            set { _config.ConfigData["Secret"] = value; }
+        }
+
+        public bool IsLogedIn
+        {
+            get { return _isLogedIn; }
+            set
+            {
+                _isLogedIn = value;
+                RaisePropertyChanged(() => IsLogedIn);
+                RaisePropertyChanged(() => IsLogedOut);
+            }
+        }
+
+        public bool IsLogedOut
+        {
+            get { return !IsLogedIn; }
+        }
+
+        public string UserName
+        {
+            get { return _config.ConfigData["UserName"]; }
+            set
+            {
+                _config.ConfigData["UserName"] = value;
+                RaisePropertyChanged(() => UserName);
+            }
+        }
 
         public DropboxViewModel()
         {
@@ -28,24 +74,69 @@ namespace CameraControl.Plugins.AutoExportPlugins
         {
             _config = configData;
             LoginCommand = new RelayCommand(Login);
+            LogoutCommand = new RelayCommand(LogOut);
+            LoadData();
+        }
+
+        private void LogOut()
+        {
+            AccessToken = "";
         }
 
         private void Login()
         {
-            _client.GetToken();
-            var url = _client.BuildAuthorizeUrl(Server);
-            _miniWebServer = new MiniWebServer(SendResponse, Server);
-            _miniWebServer.Run();
-            PhotoUtils.Run(url);
+            try
+            {
+                _client.GetToken();
+                var url = _client.BuildAuthorizeUrl(Server);
+                _miniWebServer = new MiniWebServer(SendResponse, Server);
+                _miniWebServer.Run();
+                PhotoUtils.Run(url);
+            }
+            catch (Exception e)
+            {
+                Log.Error("Unable to login");
+            }
         }
 
         public string SendResponse(HttpListenerRequest request)
         {
             var accessToken = _client.GetAccessToken();
             Application.Current.Dispatcher.Invoke(new Action(() => ((Window)ServiceProvider.PluginManager.SelectedWindow).Activate()));
+            Secret = accessToken.Secret;
+            AccessToken = accessToken.Token;
 
             return "<HTML><BODY>OK</br><h3>Logis succeed. Please return to digiCamControl</br><a href=\"javascript:window.open('','_self').close();\">close</a></BODY></HTML>";
             return string.Format("<HTML><BODY>Something goes wrong. Please try again</BODY></HTML>");
+        }
+
+        public void Upload(string file, string folder)
+        {
+            LoadData();
+            using (Stream stream = File.Open(file,FileMode.Open,FileAccess.Read))
+            {
+                _client.UploadFile(folder, Path.GetFileName(file), stream);
+            }
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(AccessToken))
+                {
+                    _client.GetToken();
+                    _client.UserLogin.Secret = Secret;
+                    _client.UserLogin.Token = AccessToken;
+                    var data = _client.AccountInfo();
+                    UserName = data.display_name;
+                    IsLogedIn = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                IsLogedIn = false;
+            }
         }
     }
 }
