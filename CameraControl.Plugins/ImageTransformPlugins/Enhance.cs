4d36fcd59aa1c7ecd59531b611a20ce7e8de8b83
Duka Istvan <dukusi@gmail.com>
2015-09-28 16:47:02 +0300

    More Enhance plugin options

--- CameraControl.Plugins/ImageTransformPlugins/Enhance.cs
+++ CameraControl.Plugins/ImageTransformPlugins/Enhance.cs
@@ -23,7 +23,17 @@ namespace CameraControl.Plugins.ImageTransformPlugins
             dest = Path.Combine(Path.GetDirectoryName(dest), Path.GetFileNameWithoutExtension(dest) + ".jpg");
             using (MagickImage image = new MagickImage(infile))
             {
+                if (conf.Normalize)
+                    image.Normalize();
+                if (conf.AutoGamma)
+                    image.AutoGamma();
                 image.BrightnessContrast(new Percentage(conf.Brightness), new Percentage(conf.Contrast));
+                if (conf.SContrast > 0)
+                    image.SigmoidalContrast(true, conf.SContrast);
+                if (conf.Edge)
+                    image.AdaptiveSharpen();
+                if (conf.Sharpen > 0)
+                    image.Unsharpmask(1.5, 1.5, conf.Sharpen/100.0, 0.2);
                 image.Format = MagickFormat.Jpeg;
                 image.Write(dest);
             }
--- CameraControl.Plugins/ImageTransformPlugins/EnhanceViewModel.cs
+++ CameraControl.Plugins/ImageTransformPlugins/EnhanceViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using CameraControl.Core.Classes;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace CameraControl.Plugins.ImageTransformPlugins
 {
@@ -12,16 +13,76 @@ namespace CameraControl.Plugins.ImageTransformPlugins
     {
         private ValuePairEnumerator _config = new ValuePairEnumerator();
 
+        public RelayCommand ResetCommand { get; set; }
+
         public int Brightness
         {
             get { return GetInt(_config["Brightness"]); }
-            set { _config["Brightness"] = value.ToString(CultureInfo.InvariantCulture); }
+            set
+            {
+                _config["Brightness"] = value.ToString(CultureInfo.InvariantCulture);
+                RaisePropertyChanged(() => Brightness);
+            }
         }
 
         public int Contrast
         {
             get { return GetInt(_config["Contrast"]); }
-            set { _config["Contrast"] = value.ToString(CultureInfo.InvariantCulture); }
+            set
+            {
+                _config["Contrast"] = value.ToString(CultureInfo.InvariantCulture);
+                RaisePropertyChanged(() => Contrast);
+            }
+        }
+
+        public int Sharpen
+        {
+            get { return GetInt(_config["Sharpen"]); }
+            set
+            {
+                _config["Sharpen"] = value.ToString(CultureInfo.InvariantCulture);
+                RaisePropertyChanged(() => Sharpen);
+            }
+        }
+
+        public double SContrast
+        {
+            get { return Getdouble(_config["SContrast"]); }
+            set
+            {
+                _config["SContrast"] = value.ToString(CultureInfo.InvariantCulture);
+                RaisePropertyChanged(() => SContrast);
+            }
+        }
+
+        public bool Normalize
+        {
+            get { return _config["Normalize"] == "True"; }
+            set
+            {
+                _config["Normalize"] = value.ToString();
+                RaisePropertyChanged(() => Normalize);
+            }
+        }
+
+        public bool AutoGamma
+        {
+            get { return _config["AutoGamma"] == "True"; }
+            set
+            {
+                _config["AutoGamma"] = value.ToString();
+                RaisePropertyChanged(() => AutoGamma);
+            }
+        }
+
+        public bool Edge
+        {
+            get { return _config["Edge"] == "True"; }
+            set
+            {
+                _config["Edge"] = value.ToString();
+                RaisePropertyChanged(() => Edge);
+            }
         }
 
         public EnhanceViewModel()
@@ -32,6 +93,18 @@ namespace CameraControl.Plugins.ImageTransformPlugins
         public EnhanceViewModel(ValuePairEnumerator config)
         {
             _config = config;
+            ResetCommand = new RelayCommand(Reset);
+        }
+
+        private void Reset()
+        {
+            Brightness = 0;
+            Contrast = 0;
+            SContrast = 0;
+            Sharpen = 0;
+            Normalize = false;
+            Edge = false;
+
         }
 
         private int GetInt(string s)
@@ -39,6 +112,13 @@ namespace CameraControl.Plugins.ImageTransformPlugins
             if (string.IsNullOrEmpty(s))
                 return 0;
             return Convert.ToInt32(s, CultureInfo.InvariantCulture);
+        }
+
+        private double Getdouble(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+            return Convert.ToDouble(s, CultureInfo.InvariantCulture);
         }
     }
 }
