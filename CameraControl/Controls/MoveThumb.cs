1999d04fd82a1e20009740ac5233778cc63fdb02
Duka Istvan <dukusi@gmail.com>
2015-10-05 16:30:47 +0300

    Replace ruler in live view Part I ref #138

---
+++ CameraControl/Controls/MoveThumb.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+// code source: http://www.codeproject.com/Articles/22952/WPF-Diagram-Designer-Part-1
+namespace CameraControl.Controls
+{
+    public class MoveThumb : Thumb
+    {
+        public MoveThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.MoveThumb_DragDelta);
+        }
+
+        private void MoveThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control designerItem = this.DataContext as Control;
+
+            if (designerItem != null)
+            {
+                double left = Canvas.GetLeft(designerItem);
+                double top = Canvas.GetTop(designerItem);
+
+                Canvas.SetLeft(designerItem, left + e.HorizontalChange);
+                Canvas.SetTop(designerItem, top + e.VerticalChange);
+            }
+        }
+    }
+}
---
+++ CameraControl/Controls/ResizeThumb.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+// code source: http://www.codeproject.com/Articles/22952/WPF-Diagram-Designer-Part-1
+namespace CameraControl.Controls
+{
+    public class ResizeThumb : Thumb
+    {
+        public ResizeThumb()
+        {
+            DragDelta += new DragDeltaEventHandler(this.ResizeThumb_DragDelta);
+        }
+
+        private void ResizeThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            Control designerItem = this.DataContext as Control;
+
+            if (designerItem != null)
+            {
+                double deltaVertical, deltaHorizontal;
+
+                switch (VerticalAlignment)
+                {
+                    case VerticalAlignment.Bottom:
+                        deltaVertical = Math.Min(-e.VerticalChange, designerItem.ActualHeight - designerItem.MinHeight);
+                        designerItem.Height -= deltaVertical;
+                        break;
+                    case VerticalAlignment.Top:
+                        deltaVertical = Math.Min(e.VerticalChange, designerItem.ActualHeight - designerItem.MinHeight);
+                        Canvas.SetTop(designerItem, Canvas.GetTop(designerItem) + deltaVertical);
+                        designerItem.Height -= deltaVertical;
+                        break;
+                    default:
+                        break;
+                }
+
+                switch (HorizontalAlignment)
+                {
+                    case HorizontalAlignment.Left:
+                        deltaHorizontal = Math.Min(e.HorizontalChange, designerItem.ActualWidth - designerItem.MinWidth);
+                        Canvas.SetLeft(designerItem, Canvas.GetLeft(designerItem) + deltaHorizontal);
+                        designerItem.Width -= deltaHorizontal;
+                        break;
+                    case HorizontalAlignment.Right:
+                        deltaHorizontal = Math.Min(-e.HorizontalChange, designerItem.ActualWidth - designerItem.MinWidth);
+                        designerItem.Width -= deltaHorizontal;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            e.Handled = true;
+        }
+    }
+}
