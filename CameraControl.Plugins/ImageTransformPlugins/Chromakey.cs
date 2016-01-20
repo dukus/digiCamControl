411f6267cf56b04598eb2b74952f7da413f1e3be
Duka Istvan <dukusi@gmail.com>
2016-01-20 21:27:38 +0200

    ChromaKey transform plugin ref #168

--- CameraControl.Plugins/ImageTransformPlugins/Chromakey.cs
+++ CameraControl.Plugins/ImageTransformPlugins/Chromakey.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Controls;
 using CameraControl.Core.Classes;
 using CameraControl.Core.Interfaces;
+using Kaliko.ImageLibrary;
+using Kaliko.ImageLibrary.Filters;
+using Kaliko.ImageLibrary.Scaling;
 
 namespace CameraControl.Plugins.ImageTransformPlugins
 {
@@ -17,7 +22,24 @@ namespace CameraControl.Plugins.ImageTransformPlugins
 
         public string Execute(FileItem item, string infile, string dest, ValuePairEnumerator configData)
         {
-            return "";
+            var conf = new ChromakeyViewModel(configData);
+            dest = Path.Combine(Path.GetDirectoryName(dest), Path.GetFileNameWithoutExtension(dest) + ".jpg");
+
+            KalikoImage image = new KalikoImage(infile);
+            var x = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString(conf.BackgroundColor);
+            var filter = new ChromaKeyFilter();
+            filter.KeyColor = Color.FromArgb(x.R, x.G, x.B);
+            filter.ToleranceHue = conf.Hue;
+            filter.ToleranceSaturnation = conf.Saturnation/100f;
+            filter.ToleranceBrightness = conf.Brigthness / 100f;
+            image.ApplyFilter(filter);
+
+            var backdrop = new KalikoImage(conf.BackgroundFile);
+            backdrop = backdrop.Scale(new FitScaling(image.Width, image.Height));
+            backdrop.BlitImage(image);
+
+            backdrop.SaveJpg(dest, 90);
+            return dest;
         }
 
         public UserControl GetConfig(ValuePairEnumerator configData)
--- CameraControl.Plugins/ImageTransformPlugins/ChromakeyViewModel.cs
+++ CameraControl.Plugins/ImageTransformPlugins/ChromakeyViewModel.cs
@@ -1,16 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using CameraControl.Core.Classes;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Win32;
 
 namespace CameraControl.Plugins.ImageTransformPlugins
 {
     public class ChromakeyViewModel : ViewModelBase
     {
         private ValuePairEnumerator _config = new ValuePairEnumerator();
-        
+        public RelayCommand BrowseCommand { get; set; }
+
+
+        public string BackgroundColor
+        {
+            get { return _config["BackgroundColor"] ?? "Green"; }
+            set
+            {
+                _config["FontColor"] = value;
+                RaisePropertyChanged(() => BackgroundColor);
+            }
+        }
+
+        public int Hue 
+        {
+            get { return GetInt(_config["Hue"]); }
+            set { _config["Hue"] = value.ToString(CultureInfo.InvariantCulture); }
+        }
+
+        public int Saturnation
+        {
+            get { return GetInt(_config["Saturnation"]); }
+            set { _config["Saturnation"] = value.ToString(CultureInfo.InvariantCulture); }
+        }
+
+        public int Brigthness 
+        {
+            get { return GetInt(_config["Brigthness"]); }
+            set { _config["Brigthness"] = value.ToString(CultureInfo.InvariantCulture); }
+        }
+
+        public string BackgroundFile
+        {
+            get { return _config["BackgroundFile"]; }
+            set
+            {
+                _config["BackgroundFile"] = value;
+                RaisePropertyChanged(() => BackgroundFile);
+            }
+        }
+
         public ChromakeyViewModel()
         {
             
@@ -19,7 +62,30 @@ namespace CameraControl.Plugins.ImageTransformPlugins
         public ChromakeyViewModel(ValuePairEnumerator config)
         {
             _config = config;
+            // first run set default values
+            if (Hue == 0 && Saturnation == 0 && Brigthness == 0)
+            {
+                Hue = 40;
+                Saturnation = 70;
+                Brigthness = 50;
+            }
+            BrowseCommand = new RelayCommand(Browse);
         }
 
+        private int GetInt(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return 0;
+            return Convert.ToInt32(s, CultureInfo.InvariantCulture);
+        }
+
+        private void Browse()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                BackgroundFile = dialog.FileName;
+            }
+        }
     }
 }
