809a083f0e214eb8092ee4869444bbadbda58014
Duka Istvan <dukusi@gmail.com>
2016-05-07 14:41:23 +0300

    2.0.27 beta release

--- Setup/setup.cs
+++ Setup/setup.cs
@@ -1,7 +1,7 @@
 using System  ;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
+using System.IO; 
 using System.Security.AccessControl;
 using System.Security.Principal;
 using System.Xml;
