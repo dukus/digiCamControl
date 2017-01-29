ea0b9955c0005ab9d844e0d979d5ae940b2e4261
Duka Istvan <dukusi@gmail.com>
2017-01-29 21:46:02 +0200

    Finished plugin editor

---
+++ CameraControl.PluginManager/Classes/ProjectFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CameraControl.Core.Plugin;
+using CameraControl.Devices.Classes;
+
+namespace CameraControl.PluginManager.Classes
+{
+    public class ProjectFile
+    {
+        public PluginInfo PluginInfo { get; set; }
+        public AsyncObservableCollection<ProjectFileItem> Files { get; set; }
+
+        public ProjectFile()
+        {
+            Files = new AsyncObservableCollection<ProjectFileItem>();
+        }
+
+    }
+}
