9c6b6e6fd47b66176014b23df75d6ab69e034917
Duka Istvan <dukusi@gmail.com>
2017-02-25 19:01:15 +0200

    Branding option for main menu

--- CameraControl.Devices/Properties/AssemblyInfo.cs
+++ CameraControl.Devices/Properties/AssemblyInfo.cs
@@ -68,5 +68,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.71.5")]
-[assembly: AssemblyFileVersion("2.0.71.5")]
+[assembly: AssemblyVersion("2.0.71.6")]
+[assembly: AssemblyFileVersion("2.0.71.6")]
