5c39d62ea0ce6d6794ffdd3ad909c8fda49cfe8c
Duka Istvan <dukusi@gmail.com>
2017-03-12 12:25:27 +0200

    CustomProperty implementation

---
+++ Capture.Workflow.Core/Classes/CustomProperty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture.Workflow.Core.Classes
+{
+    public class CustomProperty
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Parameter { get; set; }
+        public CustomPropertyType PropertyType { get; set; }
+        public List<string> ValueList { get; set; }
+        public double RangeMin { get; set; }
+        public double RangeMax { get; set; }
+
+        public CustomProperty()
+        {
+            ValueList = new List<string>();
+        }
+    }
+}
---
+++ Capture.Workflow.Core/Classes/CustomPropertyCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capture.Workflow.Core.Classes
+{
+    public class CustomPropertyCollection
+    {
+        public List<CustomProperty> Items { get; set; }
+
+        public CustomProperty this[string name]
+        {
+            get
+            {
+                foreach (var item in Items)
+                {
+                    if (item.Name == name)
+                        return item;
+                }
+                return null;
+            }
+        }
+        
+        public CustomPropertyCollection()
+        {
+            Items = new List<CustomProperty>();
+        }
+    }
+}
