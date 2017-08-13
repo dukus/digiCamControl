1b7551aaa6fbdc7d00154b58b0c83773458bd3ba
Duka Istvan <dukusi@gmail.com>
2017-08-13 10:35:01 +0300

    Support for D5 and D500 quality settings

--- CameraControl.Devices/Nikon/NikonD500.cs
+++ CameraControl.Devices/Nikon/NikonD500.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace CameraControl.Devices.Nikon
 {
-    class NikonD500
+    public class NikonD500: NikonD600Base
     {
+        protected override void InitCompressionSetting()
+        {
+            _csTable = new Dictionary<int, string>()
+            {
+                {0, "JPEG (BASIC)"},
+                {1, "JPEG (BASIC*)"},
+                {2, "JPEG (NORMAL)"},
+                {3, "JPEG (NORMAL*)"},
+                {4, "JPEG (FINE)"},
+                {5, "JPEG (FINE*)"},
+                {6, "TIFF (RGB)"},
+                {7, "RAW"},
+                {8, "RAW + JPEG (BASIC)"},
+                {9, "RAW + JPEG (BASIC*)"},
+                {10, "RAW + JPEG (NORMAL)"},
+                {11, "RAW + JPEG (NORMAL*)"},
+                {12, "RAW + JPEG (FINE)"},
+                {13, "RAW + JPEG (FINE*)"}
+            };
+            base.InitCompressionSetting();
+        }
     }
 }
