7cf740f83f6a9dbce3c3c991cd4e47eb7cfa8cf4
Duka Istvan <dukusi@gmail.com>
2017-10-19 14:41:05 +0300

    Slider element

--- Capture.Workflow.Plugins/ViewElements/SliderElement.cs
+++ Capture.Workflow.Plugins/ViewElements/SliderElement.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using Capture.Workflow.Core.Classes;
 using Capture.Workflow.Core.Classes.Attributes;
@@ -11,8 +16,8 @@ namespace Capture.Workflow.Plugins.ViewElements
 {
     [Description("")]
     [PluginType(PluginType.ViewElement)]
-    [DisplayName("Text")]
-    public class TextElement: IViewElementPlugin
+    [DisplayName("Slider")]
+    public class SliderElement : IViewElementPlugin
     {
         public string Name { get; set; }
         public WorkFlowViewElement CreateElement(WorkFlowView view)
@@ -32,11 +37,6 @@ namespace Capture.Workflow.Plugins.ViewElements
             });
             element.Properties.Items.Add(new CustomProperty()
             {
-                Name = "IsReadOnly",
-                PropertyType = CustomPropertyType.Bool
-            });
-            element.Properties.Items.Add(new CustomProperty()
-            {
                 Name = "Width",
                 PropertyType = CustomPropertyType.Number,
                 RangeMin = 0,
@@ -53,6 +53,14 @@ namespace Capture.Workflow.Plugins.ViewElements
             });
             element.Properties.Items.Add(new CustomProperty()
             {
+                Name = "FontSize",
+                PropertyType = CustomPropertyType.Number,
+                RangeMin = 6,
+                RangeMax = 400,
+                Value = "15"
+            });
+            element.Properties.Items.Add(new CustomProperty()
+            {
                 Name = "Height",
                 PropertyType = CustomPropertyType.Number,
                 RangeMin = 0,
@@ -63,7 +71,7 @@ namespace Capture.Workflow.Plugins.ViewElements
             {
                 Name = "Orientation",
                 PropertyType = CustomPropertyType.ValueList,
-                ValueList = new List<string>() {"Horizontal", "Vertical"},
+                ValueList = new List<string>() { "Horizontal", "Vertical" },
                 Value = "Horizontal"
             });
             element.Properties.Items.Add(new CustomProperty()
@@ -76,56 +84,47 @@ namespace Capture.Workflow.Plugins.ViewElements
             });
             element.Properties.Items.Add(new CustomProperty()
             {
-                Name = "FontSize",
-                PropertyType = CustomPropertyType.Number,
-                RangeMin = 6,
-                RangeMax = 400,
-                Value = "15"
-            });
-            element.Properties.Items.Add(new CustomProperty()
-            {
-                Name = "BackgroundColor",
-                PropertyType = CustomPropertyType.Color,
-                Value = "Transparent"
-            });
-            element.Properties.Items.Add(new CustomProperty()
-            {
-                Name = "ForegroundColor",
-                PropertyType = CustomPropertyType.Color,
-                Value = "Transparent"
-            });
-            element.Properties.Items.Add(new CustomProperty()
-            {
                 Name = "Variable",
                 PropertyType = CustomPropertyType.Variable,
                 Value = ""
             });
+            element.Properties.Items.Add(new CustomProperty()
+            {
+                Name = "Minimum",
+                PropertyType = CustomPropertyType.Number,
+                RangeMin = int.MinValue,
+                RangeMax = int.MaxValue,
+                Value = "-100"
+            });
+            element.Properties.Items.Add(new CustomProperty()
+            {
+                Name = "Maximum",
+                PropertyType = CustomPropertyType.Number,
+                RangeMin = int.MinValue,
+                RangeMax = int.MaxValue,
+                Value = "100"
+            });
+
             return element;
         }
 
-        public FrameworkElement GetControl(WorkFlowViewElement viewElement,Context context)
+        public FrameworkElement GetControl(WorkFlowViewElement viewElement, Context context)
         {
-            var textBox = new TextBox()
+            var slider = new Slider()
             {
-                FontSize = viewElement.Properties["FontSize"].ToInt(context),
+                Orientation = Orientation.Horizontal,
                 VerticalContentAlignment = VerticalAlignment.Center,
                 HorizontalContentAlignment = HorizontalAlignment.Center,
-                IsReadOnly = viewElement.Properties["IsReadOnly"].ToBool(context),
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
             };
-            viewElement.SetSize(textBox,context);
+            viewElement.SetSize(slider, context);
 
-            textBox.DataContext = viewElement.Parent.Parent.Variables[viewElement.Properties["Variable"].ToString(context)];
-            textBox.SetBinding(TextBox.TextProperty, "Value");
+            slider.DataContext = viewElement.Parent.Parent.Variables[viewElement.Properties["Variable"].ToString(context)];
+            slider.SetBinding(RangeBase.ValueProperty, "Value");
 
-            if (viewElement.Properties["BackgroundColor"].ToString(context) != "Transparent" && viewElement.Properties["BackgroundColor"].ToString(context) != "#00FFFFFF")
-                textBox.Background =
-                    new SolidColorBrush(
-                        (Color)ColorConverter.ConvertFromString(viewElement.Properties["BackgroundColor"].ToString(context)));
-
-            if (viewElement.Properties["ForegroundColor"].ToString(context) != "Transparent" && viewElement.Properties["ForegroundColor"].ToString(context) != "#00FFFFFF")
-                textBox.Foreground =
-                    new SolidColorBrush(
-                        (Color)ColorConverter.ConvertFromString(viewElement.Properties["ForegroundColor"].ToString(context)));
+            slider.Minimum= viewElement.Properties["Minimum"].ToInt(context);
+            slider.Maximum = viewElement.Properties["Maximum"].ToInt(context);
 
             var label = new System.Windows.Controls.Label()
             {
@@ -134,23 +133,16 @@ namespace Capture.Workflow.Plugins.ViewElements
                 Margin = new Thickness(viewElement.Properties["Margins"].ToInt(context)),
                 FontSize = viewElement.Properties["FontSize"].ToInt(context),
                 VerticalContentAlignment = VerticalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
             };
 
             if (viewElement.Properties["LabelWidth"].ToInt(context) > 0)
                 label.Width = viewElement.Properties["LabelWidth"].ToInt(context);
 
-            //if (viewElement.Properties["BackgroundColor"].Value != "Transparent" && viewElement.Properties["BackgroundColor"].Value != "#00FFFFFF")
-            //    label.Background =
-            //        new SolidColorBrush(
-            //            (Color)ColorConverter.ConvertFromString(viewElement.Properties["BackgroundColor"].Value));
-            if (viewElement.Properties["ForegroundColor"].ToString(context) != "Transparent" && viewElement.Properties["ForegroundColor"].ToString(context) != "#00FFFFFF")
-                label.Foreground =
-                    new SolidColorBrush(
-                        (Color)ColorConverter.ConvertFromString(viewElement.Properties["ForegroundColor"].ToString(context)));
 
             var stackpanel = new StackPanel();
             stackpanel.Children.Add(label);
-            stackpanel.Children.Add(textBox);
+            stackpanel.Children.Add(slider);
             stackpanel.Orientation = viewElement.Properties["Orientation"].ToString(context) == "Horizontal" ? Orientation.Horizontal : Orientation.Vertical;
             return stackpanel;
         }
