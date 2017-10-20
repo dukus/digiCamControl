d1c1a1777d9539639dc8d9bb2ad321436cc6db89
Duka Istvan <dukusi@gmail.com>
2017-10-20 12:04:35 +0300

    * Rotate action * Grouping with expander for elements

---
+++ Capture.Workflow.Plugins/Commands/ImageProcessing/RotateAction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capture.Workflow.Core.Classes;
+using Capture.Workflow.Core.Classes.Attributes;
+using Capture.Workflow.Core.Interface;
+using ImageMagick;
+
+namespace Capture.Workflow.Plugins.Commands.ImageProcessing
+{
+    [Description("")]
+    [PluginType(PluginType.Command)]
+    [DisplayName("Rotate")]
+    [Group("ImageProcessing")]
+    public class RotateAction : BaseCommand, IWorkflowCommand
+    {
+        public WorkFlowCommand CreateCommand()
+        {
+            var command = GetCommand();
+            command.Properties.Add(new CustomProperty()
+            {
+                Name = "Angle",
+                PropertyType = CustomPropertyType.Variable,
+                Value = "0"
+
+            });
+            return command;
+        }
+
+        public bool Execute(WorkFlowCommand command, Context context)
+        {
+            if (context?.ImageStream == null)
+                return true;
+            if (!CheckCondition(command, context))
+                return true;
+            context.ImageStream.Seek(0, SeekOrigin.Begin);
+            using (MagickImage image = new MagickImage(context.ImageStream))
+            {
+                context.ImageStream.Seek(0, SeekOrigin.Begin);
+                image.Rotate((double)context.WorkFlow.Variables[command.Properties["Angle"].ToString(context)].GetAsObject());
+                image.Write(context.ImageStream, MagickFormat.Jpg);
+            }
+            return true;
+        }
+    }
+}
