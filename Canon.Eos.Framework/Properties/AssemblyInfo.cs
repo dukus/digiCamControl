f5aa9d78d1fb0b80996254ced3c4fb5a9d5a876b
Duka Istvan <dukusi@gmail.com>
2022-08-22 09:54:47 +0300

    * Option to start minimized on startup

--- Canon.Eos.Framework/Properties/AssemblyInfo.cs
+++ Canon.Eos.Framework/Properties/AssemblyInfo.cs
@@ -12,6 +12,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3a70507d-c50a-47ca-b920-303d687631ec")]
-[assembly: AssemblyVersion("2.1.4.3")]
-[assembly: AssemblyFileVersion("2.1.4.3")]
+[assembly: AssemblyVersion("2.1.4.4")]
+[assembly: AssemblyFileVersion("2.1.4.4")]
 [assembly: InternalsVisibleTo("Canon.Eos.UnitTests")]
