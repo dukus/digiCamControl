3ea2e93f3221ae30d22dddb3ca3f0e8e0a555f36
mistachkin <joe@mistachkin.com>
2022-09-12 20:31:40 -0400

    Fix typo in TclScriptManager class name.

--- CameraControl/MainWindow.xaml.cs
+++ CameraControl/MainWindow.xaml.cs
@@ -368,7 +368,7 @@ namespace CameraControl
                 {
                     try
                     {
-                        var manager = new TclScripManager();
+                        var manager = new TclScriptManager();
                         manager.Execute(File.ReadAllText(scriptFile));
                     }
                     catch (Exception exception)
